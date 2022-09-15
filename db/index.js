ee71cdcab1981f7cf72bcce2ece6d2f8033c1def
ronoc123 <98913680+ronoc123@users.noreply.github.com>
2022-09-15 11:02:05 -0500

    heroku

--- db/index.js
+++ db/index.js
@@ -1,6 +1,24 @@
 import * as pg from "pg";
 const { Pool } = pg.default;
-const pool = new Pool();
+
+const devConfig = {
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  port: process.env.PG_PORT,
+};
+
+// const devConfig = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:`
+
+const proConfig = {
+  connectionString: process.env.DATABASE_URL,
+};
+
+const pool = new Pool({
+  connectionString:
+    process.env.NODE_ENV === "production" ? proConfig : devConfig,
+});
 export default {
   query: (text, params) => pool.query(text, params),
 };
